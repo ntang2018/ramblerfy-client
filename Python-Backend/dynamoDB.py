3b782e0f3fd7fa053c05c352f7dd2a0365aad09c
Alexander Rose <acrosemail@gmail.com>
2020-03-23 17:01:03 -0500

    bad security practice

--- Python-Backend/dynamoDB.py
+++ Python-Backend/dynamoDB.py
@@ -1,7 +1,8 @@
 import pymongo as pymongo
 
+# Change it to your username and password
 client = pymongo.MongoClient(
-    "mongodb+srv://arose5:ZaraYaqob14$3@ramblerpy-5rd9x.mongodb.net/test?retryWrites=true&w=majority")
+    "mongodb+srv://<username>:<password>@ramblerpy-5rd9x.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test
 
 # {tracks: {$ne: '{tracks: {"danceabilty>0.7"}'}}
