8fb1eef6a864d0f21e7537d275b9ed2b66fe97d4
Alexander Rose <acrosemail@gmail.com>
2020-03-23 16:59:34 -0500

    Changed readMe to relate to backend, example filters for MongoDB

--- Python-Backend/dynamoDB.py
+++ Python-Backend/dynamoDB.py
@@ -1,2 +1,8 @@
-''' TODO
-Write to DyanmoDB instead of a json file '''
+import pymongo as pymongo
+
+client = pymongo.MongoClient(
+    "mongodb+srv://arose5:ZaraYaqob14$3@ramblerpy-5rd9x.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
+
+# {tracks: {$ne: '{tracks: {"danceabilty>0.7"}'}}
+# $filter: {input: "$items",as: "item", cond: { $gte: [ "$$item.price", 100 ] }
